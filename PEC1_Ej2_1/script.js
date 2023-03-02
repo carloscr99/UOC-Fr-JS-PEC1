b55869c23ec84d0e900616ae30b6b84cd0e7c3bf
[Carlos Colomer Revert] <[ccolomerrev@uoc.edu]>
2023-03-02 15:48:18 +0100

    añadido campo edad y las validaciones sobre el Password

--- PEC1_Ej2_1/script.js
+++ PEC1_Ej2_1/script.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
+const age = document.getElementById('age');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
@@ -26,6 +27,17 @@ function checkEmail(input) {
     else
         showError(input, 'Email is not valid')
 
+}
+
+function checkPassword(input) {
+    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~`!@#$%^&*()_+\-=\{\}\|\[\]\\:";'<>?,.])[A-Za-z\d~`!@#$%^&*()_+\-=\{\}\|\[\]\\:";'<>?,.]{8,}$/;
+
+    console.log(input.value);
+
+    if (re.test(String(input.value)))
+        showSuccess(input);
+    else
+        showError(input, 'Password not valid')
 }
 
 // Check required fields
@@ -54,6 +66,23 @@ function checkPasswordsMatch(input1, input2) {
 
 }
 
+function checkAge(input) {
+    let minAge = 0;
+    let maxAge = 1000;
+
+    if (input.value.trim() === '')
+        showError(input, `${getFieldName(input)} is required`)
+    else {
+        if (input.value < minAge)
+            showError(input, `${getFieldName(input)} must be at least ${minAge} `);
+        else if (input.value >= maxAge)
+            showError(input, `${getFieldName(input)} must be less than ${maxAge} `);
+        else
+            showSuccess(input);
+    }
+
+}
+
 function getFieldName(input) {
     // Get the first character and make it uppercase, then concatenate the rest using the slice function starting from the specified index.
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -67,9 +96,11 @@ form.addEventListener('submit', function (e) {
     checkRequired([username, email, password, password2]);
 
     checkLength(username, 3, 15);
-    checkLength(password, 6, 25);
+    checkLength(password, 8, 25);
     checkEmail(email);
+    checkPassword(password);
     checkPasswordsMatch(password, password2);
+    checkAge(age);
 
 
 });
